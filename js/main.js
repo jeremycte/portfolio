8f8e24bd183dc06098070515c851380889548e13
Jeremy Chan <jeremyintern@convertium.com>
2019-10-30 18:37:22 +0800

    solved masonry problem

--- js/main.js
+++ js/main.js
@@ -126,17 +126,12 @@
     $grid
       .imagesLoaded()
       .always(function(instance) {
-        console.log("all images loaded");
       })
       .done(function(instance) {
-        console.log("all images successfully loaded");
       })
       .fail(function() {
-        console.log("all images loaded, at least one is broken");
       })
       .progress(function(instance, image) {
-        var result = image.isLoaded ? "loaded" : "broken";
-        console.log("image is " + result + " for " + image.img.src);
       });
 
     $grid.imagesLoaded(function() {
