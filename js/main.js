9cdedde62215b89c3725d7e7f74fb1eebf11d2bf
Jeremy Chan <jawarome99@gmail.com>
2020-05-14 01:16:16 +0800

    changed email & phone number

--- js/main.js
+++ js/main.js
@@ -19,26 +19,26 @@
 
   /* Preloader
    * -------------------------------------------------- */
-  var ssPreloader = function() {
-    $("html").addClass("ss-preload");
+  // var ssPreloader = function() {
+  //   $("html").addClass("ss-preload");
 
-    $WIN.on("load", function() {
-      // force page scroll position to top at page refresh
-      // $('html, body').animate({ scrollTop: 0 }, 'normal');
+  //   $WIN.on("load", function() {
+  //     // force page scroll position to top at page refresh
+  //     // $('html, body').animate({ scrollTop: 0 }, 'normal');
 
-      // will first fade out the loading animation
-      $("#loader").fadeOut("slow", function() {
-        // will fade out the whole DIV that covers the website.
-        $("#preloader")
-          .delay(300)
-          .fadeOut();
-      });
+  //     // will first fade out the loading animation
+  //     $("#loader").fadeOut("slow", function() {
+  //       // will fade out the whole DIV that covers the website.
+  //       $("#preloader")
+  //         .delay(300)
+  //         .fadeOut();
+  //     });
 
-      // for hero content animations
-      $("html").removeClass("ss-preload");
-      $("html").addClass("ss-loaded");
-    });
-  };
+  //     // for hero content animations
+  //     $("html").removeClass("ss-preload");
+  //     $("html").addClass("ss-loaded");
+  //   });
+  // };
 
   /* pretty print
    * -------------------------------------------------- */
